14504f0e73a307f82f253af9bac67969590e6d70
johmstep33 <johmstep33@gmail.com>
2015-12-04 20:58:59 -0600

    Added alt(titles) to images

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //ADD YOUR CUSTOM JAVASCRIPT AND jQUERY HERE
 $('.gallery').on('click','img', function() {
    $(this).animate({
-       'width':'50vh'
+       'width':'50vh',
+       'margin':'-10%'
    },500);
    $('img').not($(this)).animate({'opacity':'0.2'}, 500);
 });
